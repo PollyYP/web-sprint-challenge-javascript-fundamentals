d676311d5f65ea51416e01dfd6862998f6b39a96
Polly Yospan <porgroovy@gmail.com>
2020-11-08 22:41:20 -0500

    All tasks finished Unit 1 Sprint 3-JavaScript Fundamentals

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,8 +16,17 @@ function myFunction() {
 myFunction();
 
 // Explanation:
+nestedFunction and internal variable are defined inside myFunction so nestedFunction can access the variable that declared in the same parent function which is myFunction.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
-function summation() {}
+
+let nums = [1, 2, 3];
+
+function summation(numb) {
+  let counter = nums.reduce((acc, val) => acc + val);
+  return counter + numb;
+}
+
+console.log(summation(4));
